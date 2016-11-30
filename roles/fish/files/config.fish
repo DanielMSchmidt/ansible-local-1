e9769164dc4dc3fb0f1b988159d8e5ee15342d9d
Daniel Schmidt <danielmschmidt92@gmail.com>
2016-11-30 19:55:48 +0100

    removed rbenv from fish config

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -9,8 +9,7 @@ set -x LANG en_US.UTF-8
 
 set -x GOPATH ~/Projects/go
 
-set -x PATH /usr/local/sbin ~/.rbenv/shims $PATH ~/Applications/bin ~/.npm/bin
-set -x PATH $PATH $GOPATH/bin ~/Projects/weluse/snippets/bin ~/.gem/ruby/2.0.0/bin
+set -x PATH /usr/local/sbin ~/.npm/bin
 
 set -x EDITOR /usr/local/bin/vim
 
