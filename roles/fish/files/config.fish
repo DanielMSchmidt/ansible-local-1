120cfb0530a4ca1b48c20cc749a88678f8141330
Daniel Schmidt <danielmschmidt92@gmail.com>
2016-12-02 14:32:10 +0100

    Update config.fish

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -7,9 +7,7 @@ set -g theme_display_ruby no
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 
-set -x GOPATH ~/Projects/go
-
-set -x PATH /usr/local/sbin
+set -x PATH /usr/local/sbin $PATH
 
 set -x EDITOR /usr/local/bin/vim
 
